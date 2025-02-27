9caf96594547fcdf00b6aa01ab9c2242b01582be
Selton Augusto <selthive@gmail.com>
2025-02-27 04:59:50 -0300

    Criação do endpoint POST /agendamento

--- api_agendamento.py
+++ api_agendamento.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from db import db
+from datetime import datetime
 from models import Agendamento
 
 app = Flask(__name__)
@@ -7,6 +8,20 @@ app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///agendamentos.db"
 db.init_app(app)
 
 # endpoint agendamento
+@app.route('/agendamento', methods=['POST'])
+def create_agendamento():
+    dados = request.get_json()
+    data_hora = datetime.now()
+    status = "AGENDADO"
+    new_agendamento = Agendamento(data_hora=data_hora, 
+                                  email_destinatario=dados['email_destinatario'], 
+                                  telefone_destinatario=dados['telefone_destinatario'], 
+                                  mensagem=dados['mensagem'],
+                                  status=status)
+    db.session.add(new_agendamento)
+    db.session.commit()
+    db.session.close()
+    return jsonify({'message': 'Agendamento efetuado com sucesso!'}), 201
 
 # endpoint consulta
 
